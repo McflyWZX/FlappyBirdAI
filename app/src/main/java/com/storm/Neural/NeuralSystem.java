46547540c1067cf3fc79544b14c98b2a6a63c677
McflyWZX <1932406470@qq.com>
2019-03-23 00:12:19 +0800

    神经网络库基本完成

--- app/src/main/java/com/storm/Neural/NeuralSystem.java
+++ app/src/main/java/com/storm/Neural/NeuralSystem.java
@@ -1,5 +1,61 @@
 package com.storm.Neural;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class NeuralSystem
 {
+	ArrayDeque<ArrayList<Neuron>> layers;
+	
+	public NeuralSystem()
+	{
+		layers = new ArrayDeque<ArrayList<Neuron>>();
+	}
+	
+	public ArrayList<Neuron> getInPut()
+	{
+		return layers.getFirst();
+	}
+	
+	public ArrayList<Neuron> caclOutPut()
+	{
+		for(ArrayList<Neuron> layer : layers )
+		{
+			if(layer != layers.getLast())
+			{
+				for(Neuron n : layer)
+				{
+					n.spread();
+				}
+			}
+		}
+		return layers.getLast();
+	}
+	
+	public void addLayer(int neurons)
+	{
+		if(layers.isEmpty())
+		{
+			ArrayList<Neuron> newL = new ArrayList<Neuron>();
+			for(int i = 0; i < neurons; i++)
+			{
+				Neuron n = new Neuron((float)(Math.random() - 0.5), (float)(Math.random() - 0.5));
+				newL.add(n);
+			}
+			layers.addLast(newL);
+			
+		} else {
+			ArrayList<Neuron> newL = new ArrayList<Neuron>(),
+						      lastL = layers.getLast();
+			for(int i = 0; i < neurons; i++)
+			{
+				Neuron n = new Neuron((float)(Math.random() - 0.5), (float)(Math.random() - 0.5));
+				for(Neuron lN : lastL)
+				{
+					lN.linkTo(n);
+				}
+				newL.add(n);
+			}
+			layers.addLast(newL);
+		}
+	}
 }
--- app/src/main/java/com/storm/Neural/Neuron.java
+++ app/src/main/java/com/storm/Neural/Neuron.java
@@ -1,5 +1,78 @@
 package com.storm.Neural;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class Neuron
 {
+	ArrayList<Neuron> toNext;
+	
+	float b, w, outFromLastLayer;//
+	
+	public Neuron()
+	{
+		b = w = outFromLastLayer = 0;
+		toNext = new ArrayList<Neuron>();
+	}
+	
+	Neuron(float b, float w)
+	{
+		this.b = b;
+		this.w = w;
+		outFromLastLayer = 0;
+		toNext = new ArrayList<Neuron>();
+	}
+
+	public void setB(float b)
+	{
+		this.b = b;
+	}
+
+	public float getB()
+	{
+		return b;
+	}
+
+	public void setW(float w)
+	{
+		this.w = w;
+	}
+
+	public float getW()
+	{
+		return w;
+	}
+
+	public void addToOutFromLastLayer(float input)
+	{
+		this.outFromLastLayer += input;
+	}
+	
+	public void setOutFromLastLayer(float outFromLastLayer)
+	{
+		this.outFromLastLayer = outFromLastLayer;
+	}
+
+	public float getOutFromLastLayer()
+	{
+		return outFromLastLayer;
+	}
+	
+	void spread()
+	{
+		for(Neuron n : toNext)
+		{
+			n.addToOutFromLastLayer(this.outFromLastLayer * w);
+		}
+		this.outFromLastLayer = 0;
+	}
+	
+	float sigmod(float x)
+	{
+		return (float)(Math.exp(x) / (Math.exp(x) + Math.exp(-x)));
+	}
+	
+	void linkTo(Neuron n)
+	{
+		toNext.add(n);
+	}
 }
